55ab5e8557f8c412620456d5bca437140fefa218
ROU001 <79861981+ROU001@users.noreply.github.com>
2021-04-01 13:31:50 -0700

    Add files via upload

---
+++ bash.sh
@@ -0,0 +1,144 @@
+git clone  https://github.com/Z4nzu/hackingtool.git
+sudo su
+sudo apt install python3
+sudo apt install python2
+sudo apt install bash
+sudo apt install fidlet
+bash install.sh
+1
+0
+sudo apt install python3
+Y
+python3 hackingtool.py
+0
+1
+99
+1
+1
+99
+0
+1
+99
+2
+1
+99
+3
+199
+4
+1
+99
+5
+1
+99
+6
+1
+99
+7
+1
+99
+8
+1
+99
+9
+1
+99
+10
+1
+99
+11
+1
+99
+12
+1
+99
+13
+1
+99
+14
+1
+99 
+0
+1
+99
+1
+1
+99
+0
+1
+99
+2
+1
+99
+3
+199
+4
+1
+99
+5
+1
+99
+6
+1
+99
+7
+1
+99
+8
+1
+99 
+0
+1
+99
+1
+1 
+99
+0
+1 
+99
+2
+1 
+99
+3
+99
+4
+1
+99
+5
+1
+99
+6
+99 
+0
+1
+99
+1
+1 
+99
+0
+1 
+99
+2
+1 
+99
+3
+199
+4
+1
+99
+5
+1
+99
+6
+1
+99
+7
+1
+99
+8
+1
+99 
+fidlet you need to install 7-17 to save space we will not install all of them Right now 
+apt-get install git
+git clone https://github.com/FajarTheGGman/F-Tools
+fidlet More Tools soon 
+fidlet ROU
